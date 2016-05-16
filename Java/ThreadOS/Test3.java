9ec6866589a82931d3717e24db7ba148cfc61569
RagT <raghutir@gmail.com>
2016-05-15 20:17:16 -0700

    Implemented Test3

--- Java/ThreadOS/Test3.java
+++ Java/ThreadOS/Test3.java
@@ -1,54 +1,42 @@
-import java.util.Date;
+import java.util.*;
 /*
 Raghu Tirumala
 Test3.java
 
 Write a user-level test thread called Test3.java which spawns and waits for the completion of X pairs of threads
 (where X = 1 ~ 4), one conducting only numerical computation and the other reading/writing many blocks randomly
-across the disk. Those two types of threads may be written in TestThread3a.java and TestThread3b.java separately or
-written in TestThread3.java that receives an argument and conducts computation or disk
-accesses according to the argument.
+across the disk.
 */
 class Test3 extends Thread {
-
     private int pairs;
-    private long startingTime;
-    private long endingTime;
+    private long startTime;
+    private long totalTime;
 
-    public Test3 ( String args[] ) {
-        pairs = Integer.parseInt( args[0] );
+    public Test3( String[] args ) {
+        pairs = Integer.parseInt(args[0]);
+        startTime =  new Date().getTime();
     }
 
-    public void run( ) {
-        String[] args1;
-        String[] args2;
+    public void run() {
+        Map<Integer,String> tasks = new HashMap<Integer,String>(); //Stores tasks executed by which thread
 
-        startingTime = (new Date( ) ).getTime( );
+        //Makes threads execute computational and disk tasks
+        for (int i = 0; i < pairs; i++) {
+            String[] thread1 = SysLib.stringToArgs("TestThread3 CompTest 10000");
+            String[] thread2 = SysLib.stringToArgs("TestThread3 DiskTest 5000");
 
-        String [] numerical = SysLib.stringToArgs("TestThread3a");
-        String [] readWrite = SysLib.stringToArgs("TestThread3b");
-        for(int i = 0; i < pairs; i++) { //Run TestTread3a
-            SysLib.exec(numerical);
+            tasks.put(SysLib.exec(thread1),"Computation"); //Executes computational task thread
+            tasks.put(SysLib.exec(thread2),"Disk Task"); //Executes Disk Read/Write Task thread
         }
 
-        for (int i = 0; i < pairs; i++) { //Run TestThread3b
-            SysLib.exec(readWrite);
+
+        for (int i = 0; i < pairs * 2; i++) {
+            String type = tasks.remove(SysLib.join());
+            SysLib.cout(type +" finished in " + ( new Date().getTime()- startTime) + "ms\n");
         }
 
-        //Numerical Work
-        for(int i = 0; i < pairs; i++) {
-            SysLib.join();
-            SysLib.cout("computation completed.\n");
-        }
-
-        //reading/writing
-        for(int i = 0; i < pairs; i++) {
-            SysLib.join();
-            SysLib.cout("read/write completed.\n");
-        }
-        endingTime = (new Date( ) ).getTime( );
-
-        SysLib.cout( "elapsed time = " + ( endingTime - startingTime ) + " msec.\n" );
-        SysLib.exit( );
+        totalTime =  new Date().getTime() - startTime;
+        SysLib.cout("elapsed time = " + totalTime + "ms\n");
+        SysLib.exit();
     }
 }
--- Java/ThreadOS/TestThread3.java
+++ Java/ThreadOS/TestThread3.java
@@ -1,62 +1,42 @@
-import java.util.Date;
+import java.util.*;
 
-class TestThread3 extends Thread {
-   private String threadName;
-   private String testName;
+public class TestThread3 extends Thread {
+    private String testType;
+    private byte[] bytes;
+    private Random rand;
+    private int num; //number of times to run operation
+    final int diskSize = 1000;
 
-   private long startTime, stopTime;
-   private long blockStartTime, blockStopTime;
-   private long testStartTime, testStopTime;
-   private String strOut;
+    public TestThread3(String[] args) {
+        testType = args[0];
+        num = Integer.parseInt(args[1]);
+        bytes = new byte[Disk.blockSize];
+        rand = new Random();
+    }
 
-   public TestThread3 ( String args[] ) {
-      testName = args[0];
-      threadName = args[1];
-   }
+    public void run() {
+        if (testType.equals("CompTest")) { //Run computation
+            RunComp();
+        } else if(testType.equals("DiskTest")) {			//Run Disk
+            RunDisk();
+        }
+        SysLib.exit();
+    }
 
-   public void run( ) {
+    // Randomly reads or writes to a random block on the disk
+    private void RunDisk() {
+        for(int i = 0; i < num; i++) {
+            int block = rand.nextInt(diskSize); //pick block
+            int option = rand.nextInt(1); //pick whether to read or write
+            if (option == 0)
+                SysLib.rawread(block, bytes);
+            else
+                SysLib.rawwrite(block, bytes);
 
-      if ( testName.equals( "Computaionally_Intensive_Test" ) ) {
-         double powerSignal, waveSignal, rmsSignal;
-         startTime = (new Date()).getTime();
-         rmsSignal = 0.0;
+        }
+    }
 
-         Integer N = (Integer.MAX_VALUE)/100;
-         for (double i = 0.0; i < (double) (N); i = i + 1.0) {
-            waveSignal = Math.sin((i / 360.0) * Math.PI);
-            powerSignal = Math.pow(waveSignal, 2.0);
-            rmsSignal = rmsSignal + Math.sqrt(powerSignal);
-         }
-         stopTime = (new Date()).getTime();
-         SysLib.cout("\n[Thread:" + threadName + "]: ==> AvgRMS Value: "+ rmsSignal/N  +"  Computation Time: " + (stopTime - startTime) + "msec\n");
-      }
-      else if ( testName.equals( "Disk_Intensive_Test" ) ) {
-         byte[] buffer = new byte[512];
-         testStartTime = (new Date()).getTime();
-         blockStartTime = testStartTime;
-         for ( int i = 0; i < 1000; i+=10 ) {
 
-            /******************************************
-             *
-             * PERFORM YOUR DISK READ/WRITE TESTS HERE
-             *
-             ***************************************** */
-            
-            // Print out time stats for each 100 blocks tested
-            if ((i != 0) && (i % 100) == 0) {
-               blockStopTime = (new Date()).getTime();
-
-               String s = String.format("[Thread:" + threadName + "]: Disk: Block:"+ i + " of 1000 - Time: %.3f sec Avg Block Write/Read Time: %d msec%n",
-                     (double)((blockStopTime-blockStartTime)/1000.0),
-                     ((blockStopTime-blockStartTime)/100));
-               SysLib.cout(s);
-               blockStartTime = (new Date()).getTime();
-            }
-         }
-         testStopTime = (new Date()).getTime();
-         SysLib.cout("\n[Thread:" + threadName + "]: ==> Testing Disk Time: " + (testStopTime-testStartTime) + "msec\n");
-      }
-      SysLib.cout("[Thread:" + threadName + "]: " + testName + " finished...\n\n" );
-      SysLib.exit( );
-   }
+    private void RunComp() {
+    }
 }
