545c00d3847a0bc9f9798a3b6f0497e4b413070f
RagT <raghutir@gmail.com>
2016-05-15 19:31:13 -0700

    Implemented Test3

--- Java/ThreadOS/Test3.java
+++ Java/ThreadOS/Test3.java
@@ -1,32 +1,54 @@
 import java.util.Date;
+/*
+Raghu Tirumala
+Test3.java
 
+Write a user-level test thread called Test3.java which spawns and waits for the completion of X pairs of threads
+(where X = 1 ~ 4), one conducting only numerical computation and the other reading/writing many blocks randomly
+across the disk. Those two types of threads may be written in TestThread3a.java and TestThread3b.java separately or
+written in TestThread3.java that receives an argument and conducts computation or disk
+accesses according to the argument.
+*/
 class Test3 extends Thread {
 
-   private int pairs;
+    private int pairs;
+    private long startingTime;
+    private long endingTime;
 
-   public Test3 ( String args[] ) {
-      pairs = Integer.parseInt( args[0] );
-   }
+    public Test3 ( String args[] ) {
+        pairs = Integer.parseInt( args[0] );
+    }
 
-   public void run( ) {
-      String[] args1;
-      String[] args2;
+    public void run( ) {
+        String[] args1;
+        String[] args2;
 
-      long startTime = (new Date( ) ).getTime( );
-      for ( int i = 0; i < pairs; i++ ) {
+        startingTime = (new Date( ) ).getTime( );
 
-         int threadNum = i+1;
-         args1 = SysLib.stringToArgs( "TestThread3 Computaionally_Intensive_Test " + threadNum); // computation intensive
-         args2 = SysLib.stringToArgs( "TestThread3 Disk_Intensive_Test " + threadNum); 		// disk intensive
+        String [] numerical = SysLib.stringToArgs("TestThread3a");
+        String [] readWrite = SysLib.stringToArgs("TestThread3b");
+        for(int i = 0; i < pairs; i++) { //Run TestTread3a
+            SysLib.exec(numerical);
+        }
 
-         SysLib.exec( args1 );
-         SysLib.exec( args2 );
-      }
-      for (int i = 0; i < pairs * 2; i++ )
-         SysLib.join( );
-      long endTime = (new Date( ) ).getTime( );
+        for (int i = 0; i < pairs; i++) { //Run TestThread3b
+            SysLib.exec(readWrite);
+        }
 
-      SysLib.cout( "elapsed time = " + ( endTime - startTime ) + " msec.\n" );
-      SysLib.exit( );
-   }
+        //Numerical Work
+        for(int i = 0; i < pairs; i++) {
+            SysLib.join();
+            SysLib.cout("computation completed.\n");
+        }
+
+        //reading/writing
+        for(int i = 0; i < pairs; i++) {
+            SysLib.join();
+            SysLib.cout("read/write completed.\n");
+        }
+        endingTime = (new Date( ) ).getTime( );
+
+        SysLib.cout( "elapsed time = " + ( endingTime - startingTime ) + " msec.\n" );
+        SysLib.exit( );
+    }
 }
